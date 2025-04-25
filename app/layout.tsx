596865a4ca3d225c31730645fc034dc3d31d9aba
Arun Pranav A T <pranav9176@gmail.com>
2025-04-25 13:11:27 +0530

    [chore]: metadata changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,11 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Arun Pranav A T - Developer Portfolio",
+  title: "Arun Pranav A T - Software Engineer Portfolio",
   description: "Explore the personal portfolio of Arun Pranav A T - showcasing projects, skills, and my journey in tech.",
   metadataBase: new URL("https://arunpranavat.vercel.app"),
   openGraph: {
-    title: "Arun Pranav A T - Developer Portfolio",
+    title: "Arun Pranav A T - Software Engineer Portfolio",
     description: "Explore the personal portfolio of Arun Pranav A T - showcasing projects, skills, and my journey in tech.",
     url: "https://arunpranavat.vercel.app",
     siteName: "Arun Pranav A T",
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
         url: "/og-banner.png",
         width: 1200,
         height: 630,
-        alt: "Arun Pranav A T - Developer Portfolio",
+        alt: "Arun Pranav A T - Software Engineer Portfolio",
       },
     ],
     locale: "en_US",
@@ -32,9 +32,8 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Arun Pranav A T - Developer Portfolio",
+    title: "Arun Pranav A T - Software Engineer Portfolio",
     description: "Explore my personal site - built with Next.js & Tailwind CSS.",
-    images: ["/og-banner.png"],
   },
 };
 
